109161405c8a61ccd1dec260f6a33a60b31efbf8
porowns <porowns@gmail.com>
2018-03-02 21:10:17 -0500

    Initial CPU charity boilerplate

--- cpucharity/cpucharity/settings.py
+++ cpucharity/cpucharity/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'cpucharity.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'cpucharity.wsgi.application'
 
 
 # Database
